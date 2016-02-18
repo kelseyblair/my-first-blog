5b965cca1cbb623dce54ee7e3452c97c05605a84
Kelsey <kelseyblair@outlook.com>
2016-02-17 23:32:20 -0800

    okay fine, I'll not change gitignore for now and just see how the server deals with these new settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,6 +80,13 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    'server': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kelseyblair$default',
+        'USER': 'kelseyblair',
+        'PASSWORD': 'transcend167yBg',
+        'HOST': 'kelseyblair.mysql.pythonanywhere-services.com',
+    }
 }
 
 
