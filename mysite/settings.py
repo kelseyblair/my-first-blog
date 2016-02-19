e21e2676de9dab257bdf577a9ba60bf3e5bf92cc
Kelsey <kelseyblair@outlook.com>
2016-02-19 01:49:28 -0800

    Added views to create/edit blog post inside the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,13 +79,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'server': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'kelseyblair$default',
-        'USER': 'kelseyblair',
-        'PASSWORD': 'transcend167yBg',
-        'HOST': 'kelseyblair.mysql.pythonanywhere-services.com',
     }
 }
 
